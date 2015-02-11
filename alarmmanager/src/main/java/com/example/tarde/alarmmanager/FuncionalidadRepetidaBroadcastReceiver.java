507c966c0a1214e5bc4094246f4d6949dc8ec394
victorromerorodriguez <victor.rord@gmail.com>
2015-02-11 18:13:11 +0100

    Módulo AlarmManager

---
+++ alarmmanager/src/main/java/com/example/tarde/alarmmanager/FuncionalidadRepetidaBroadcastReceiver.java
@@ -0,0 +1,24 @@
+package com.example.tarde.alarmmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class FuncionalidadRepetidaBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String EXTRA_DATO = "dato";
+
+    public FuncionalidadRepetidaBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //Descarga del Fichero...
+
+        Intent intent2 = new Intent(MainActivity.ACTION_RECEIVER);
+        intent2.putExtra(EXTRA_DATO, "Te saludo una y otra vez");
+
+        context.sendBroadcast(intent2);
+    }
+}
